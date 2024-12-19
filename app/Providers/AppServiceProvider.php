8a0f99af37a55cddb7c6a9e27c9b42eeffb53044
akmaladisa <saputraakmal02@gmail.com>
2024-12-19 07:19:35 +0700

    force using https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+
     public function boot(): void
     {
-        //
+        if (config('app.env') != 'local') {
+            \URL::forceScheme('https');
+        }
     }
 }
